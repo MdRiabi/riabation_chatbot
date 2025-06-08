1a6490188a49c4ffd136c634e9ff7b1cad9245ff
MdRiabi <riabymohamed@gmail.com>
2025-06-08 15:38:04 +0100

    Add login functionality with user authentication

---
+++ views/login_view.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from models import user_model
+
+def login():
+    st.title("Se connecter")
+    username = st.text_input("Nom d'utilisateur")
+    password = st.text_input("Mot de passe", type="password")
+    if st.button("Connexion"):
+        if user_model.authenticate_user(username, password):
+            st.session_state.authenticated = True
+            st.session_state.username = username
+            st.success("Connexion réussie")
+        else:
+            st.error("Nom d'utilisateur ou mot de passe incorrect")
